1caec41d4deeca036f09f41e364d0a451a05923b
egoing <egoing@gmail.com>
2022-03-01 09:42:48 +0900

    이벤트 프로그래밍

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function Header(){
+function Header(props){
+  function clickHandler(event){
+    event.preventDefault();
+    props.onChangeMode();
+  }
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/" onClick={clickHandler}>WEB</a></h1>
   </header>
 }
 function Nav(props){
   let lis = [];
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a href={'/read/'+t.id}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -31,13 +36,17 @@ function App() {
     {id:2, title:'CSS', body:'CSS is ...'},
     {id:3, title:'JavaScript', body:'JavaScript is ...'}
   ];
+  function onChangeHeaderHandler(){
+    console.log('Welcome');
+  }
   return (
         <> 
-          <Header></Header>
+          <Header onChangeMode={onChangeHeaderHandler}></Header>
           <Nav topics={topics}></Nav>
           <Article title="Welcome" body="Hello, WEB"></Article>
         </>
   );
 }
+
 
 export default App;
