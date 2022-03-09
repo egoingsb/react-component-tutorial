f447b0e276157eb3a2a2c476f977178a7c3f29c8
egoing <egoing@gmail.com>
2022-03-09 19:19:39 +0900

    useState를 이용해서 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import {useState} from 'react';
 function App() {
+  const [count, setCount] = useState(0);
+  function increaseHandler(){
+    setCount(count+1);
+  }
+  function decreaseHandler(){
+    setCount(count-1);
+  }
+  function resetHandler(){
+    setCount(0);
+  }
   return (
        <div>
          <h1>Couter</h1>
-         <div>0</div>
-         <input type="button" value="+"></input>
-         <input type="button" value="0"></input>
-         <input type="button" value="-"></input>
+         <div>{count}</div>
+         <input type="button" value="+" onClick={increaseHandler}></input>
+         <input type="button" value="0" onClick={resetHandler}></input>
+         <input type="button" value="-" onClick={decreaseHandler}></input>
        </div>
   );
 }
